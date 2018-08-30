0507eb6784c5ddf202b15d4fa16b0c48fb82caa6
sertoi <nguyentuongvien@yahoo.com>
2018-08-30 11:18:16 +0200

    je jais class personne et participant

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.DAL
+{
+    class Personne
+    {
+        
+            public int Id { get; set; }
+            public Civil Civilite { get; set; }
+            public string Nom { get; set; }
+            public string Prenom { get; set; }
+            public string Adresse { get; set; }
+            public string Telephone { get; set; }
+            public DateTime DateNaissance { get; set; }
+            public string Potentiel { get; set; }
+            public int[] DossierVoyageIds { get; set; }
+            public int GetAge()
+            {
+                return DateTime.Today.Year - DateNaissance.Year; ;
+            }
+
+            public bool GetTarifReduit()
+            {
+                return (GetAge() < 12);
+            }
+    }
+    enum Civil { Mr, Mme, Mlle }
+}
+
---
+++ ClassLibrary1/Participant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace BoVoyage.DAL
+    {
+        class Participant:Personne
+        {
+            public int Id { get; set; }
+            public int VoyageId { get; set; }
+            public int AgeMin { get; set; }
+            public int AgeMax { get; set; }
+            public int NombreClientFavorable { get; set; }
+
+            public List<int> ClientIds { get; set; }
+
+            public void Telephoner(List<Client> Clients)
+            {
+                Console.Clear();
+                Console.WriteLine("Liste des numéros de téléphone de clients à téléphoner :  ");
+                foreach (var Client in Clients)
+                {
+                    Console.WriteLine(Client.Telephone);
+                }
+
+                Console.WriteLine("Le campagne a été effectué par téléphone avec succès ");
+            }
+
+
+        }
+    
+}
